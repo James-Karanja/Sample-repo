9aa57c6775cff5990e60d771aeff0b787cc6c3a0
James-Karanja <karanjenga@gmail.com>
2023-03-16 17:36:17 +0300

    Added modal to button

---
+++ js file.js
@@ -0,0 +1,8 @@
+const getData = async () => {
+    var result = await
+        fetch("https://jsonplaceholder.typicode.com/users");
+    var data = await result.json();
+    console.log(data);
+};
+
+getData();
