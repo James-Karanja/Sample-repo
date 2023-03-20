8638f7434e813b8cf44c6b6b5d95888343faad39
James-Karanja <karanjenga@gmail.com>
2023-03-20 14:20:40 +0300

    How to add a card

--- js file.js
+++
@@ -1,8 +0,0 @@
-const getData = async () => {
-    var result = await
-        fetch("https://jsonplaceholder.typicode.com/users");
-    var data = await result.json();
-    console.log(data);
-};
-
-getData();
